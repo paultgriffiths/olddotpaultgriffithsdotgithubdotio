23b10782cfa2afd7ce346e923f08ce47a7e55d0b
Paul Griffiths <paul.griffiths@gmail.com>
2020-02-04 09:44:53 +0700

    update 2019 talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,6 +5,21 @@ var addressPoints = [
     98.988048
   ],
   [
+    "Exeter, United Kingdom",
+    50.725562,
+    -3.5269108
+  ],
+  [
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Princeton, USA",
+    33.1801161,
+    -96.4980424
+  ],
+  [
     "Kuala Lumpur, Malaysia",
     3.1516964,
     101.6942371
