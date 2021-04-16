2946f3094ddb70f9d5aa36c2a74dda2ee572b58a
Paul Griffiths <paul.griffiths@gmail.com>
2021-04-16 17:06:06 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,23 +1,8 @@
 var addressPoints = [
   [
-    "Chiang Mai, Thailand",
-    18.790876,
-    98.988048
-  ],
-  [
-    "Exeter, United Kingdom",
-    50.725562,
-    -3.5269108
-  ],
-  [
     "Vienna, Austria",
     48.2083537,
     16.3725042
-  ],
-  [
-    "Princeton, New Jersey, USA",
-    40.3492744,
-    -74.6592958
   ],
   [
     "Birmingham, UK",
@@ -25,8 +10,33 @@ var addressPoints = [
     -1.9026911
   ],
   [
+    "Cambridge, United Kingdom",
+    52.197584649999996,
+    0.13915373736874398
+  ],
+  [
+    "Exeter, United Kingdom",
+    50.725562,
+    -3.5269108
+  ],
+  [
     "Kuala Lumpur, Malaysia",
     3.1516964,
     101.6942371
+  ],
+  [
+    "Leeds, United Kingdom",
+    53.7974185,
+    -1.5437941
+  ],
+  [
+    "Princeton, New Jersey, USA",
+    40.3492744,
+    -74.6592958
+  ],
+  [
+    "Chiang Mai, Thailand",
+    18.7882778,
+    98.9868056
   ]
 ];
